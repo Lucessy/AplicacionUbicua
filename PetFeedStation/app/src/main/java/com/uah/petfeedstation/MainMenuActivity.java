081df9e3bb16f199f10e7d977472e441f2ced0d7
javiglezzz <javier.gonzalezh@edu.uah.es>
2025-01-08 01:37:52 +0100

    Conexion http con servidor TomCat

--- PetFeedStation/app/src/main/java/com/uah/petfeedstation/MainMenuActivity.java
+++ PetFeedStation/app/src/main/java/com/uah/petfeedstation/MainMenuActivity.java
@@ -57,7 +57,7 @@ public class MainMenuActivity extends AppCompatActivity {
             currentID = getIntent().getStringExtra("registerID");
             ipVirtualMachine = getIntent().getStringExtra("ipVirtualMachine");
         }
-        Log.i(tag, ipVirtualMachine);
+
         /* Texto de bienvenida */
         TextView welcomeText = findViewById(R.id.welcome_text);
         String currentDate = new SimpleDateFormat("EEEE, dd MMMM yyyy", new Locale("es", "ES")).format(new Date());
